10fe829fcf97f19fb413d953c8f1ed747be28b8d
hjs5979 <hjs5979@gmail.com>
2023-12-16 11:29:39 +0900

    빌드 테스트

--- src/MainPage.js
+++ src/MainPage.js
@@ -11,6 +11,7 @@ import Menu from './Menu';
 export default function MainPage() {
   const apiUrl = process.env.REACT_APP_API_URL;
   console.log(apiUrl);
+  console.log(process.env);
   const [word, setWord] = useState(null);
   const [stock, setStock] = useState(null);
   const [loading, setLoading] = useState(false);
